faaabade09923fe4eba6dad49442065aa00dc55b
harish4b4 <harishguruvardhan@gmail.com>
2017-04-15 14:42:12 +0530

    First commit

---
+++ src/test/java/group/ATest.java
@@ -0,0 +1,14 @@
+package group;
+
+import org.junit.Test;
+
+/**
+ * Created by harish on 13-04-2017.
+ */
+public class ATest {
+
+    @Test
+    public void tes(){
+        System.out.println("hai this is \n harish");
+    }
+}
